3d95da0771d24a340cdfe5f6737f5de1b3b54c39
vladislavcatenate <vladislav.enev@catenate.com>
2023-08-06 13:05:58 +0300

    feat: implement react-redux-firebase and redux-firestore, eslint rules.
    Hint: Redux + Firebase hooks + Firestore

--- src/components/HomeDesktop.tsx
+++ src/components/HomeDesktop.tsx
@@ -1,9 +1,13 @@
-import {styled} from "@mui/material/styles";
-import {useEffect} from "react";
-import DefaultLayer from "../layouts/default-layer.tsx";
+import { styled } from '@mui/material/styles';
+import { useEffect } from 'react';
+import DefaultLayer from '../layouts/default-layer.tsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { Animal } from '../models/animal.model.ts';
+import { useFirestoreConnect } from 'react-redux-firebase';
+import { signIn } from '../store/actions/authActions.ts';
 
 export const HomeWrapper = styled('div')(
-    ({ theme }) => `
+  ({ theme }) => `
   display: flex;
   flex-direction: column;
   width: 100%;
@@ -19,16 +23,36 @@ export const HomeWrapper = styled('div')(
 `,
 );
 
-const HomeDesktop = () => {
-    useEffect(() => {
-        console.log('here');
-    }, []);
-
-    return (
-       <DefaultLayer>
-           <div>Hello</div>
-       </DefaultLayer>
-    );
+interface Props {
+    test?: boolean;
 }
+
+const HomeDesktop: React.FC<Props> = () => {
+  const dispatch = useDispatch();
+  // Use this hook if you want to handle data fetching with react-redux-firebase
+  useFirestoreConnect([{ collection: 'animals' }]);
+
+  // Use useSelector hook to get the data from the Redux store
+  const animals = useSelector((state: any) => state.firestore.ordered.animals) as Animal[];
+
+  useEffect(() => {
+    console.log('animals', animals);
+
+    // Auth with system account
+    //   dispatch(
+    //       // @ts-ignore
+    //   signIn({
+    //     email: 'vlad@abv.bg',
+    //     password: 'Qwerty123'
+    //   })
+    // );
+  }, []);
+
+  return (
+    <DefaultLayer>
+      <div>Hello</div>
+    </DefaultLayer>
+  );
+};
 
 export default HomeDesktop;
---
+++ src/config/fbConfig.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+export default firebase;
