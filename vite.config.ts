7c80ad5a159beb046df0ff7bd1c47c15ee173e01
Vladislav <vlladislav45@gmail.com>
2023-08-18 08:43:36 +0300

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -7,19 +7,21 @@ export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const config = {
+    optimizeDeps: { include: ['react-router-dom'] },
     plugins: [react()],
     build: {
-      rollupOptions: {
-        external: ['react', 'react-router', 'react-router-dom', 'react-redux'],
-        output: {
-          globals: {
-            'react': 'React',
-            'react-router': 'ReactRouter',
-            'react-router-dom': 'ReactRouterDOM',
-            'react-redux': 'ReactRedux',
-          },
-        },
-      },
+      // rollupOptions: {
+      //   external: ['react', 'react-dom', 'react-router', 'react-router-dom', 'react-redux'],
+      //   output: {
+      //     globals: {
+      //       'react': 'React',
+      //       'react-dom': 'ReactDOM',
+      //       'react-router': 'ReactRouter',
+      //       'react-router-dom': 'ReactRouterDOM',
+      //       'react-redux': 'ReactRedux',
+      //     },
+      //   },
+      // },
     },
   };
 
